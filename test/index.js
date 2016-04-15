37b2c2fadba7a954ef7d1e4f42a8023cc4ed44e1
Ryan Tsao <rtsao@uber.com>
2016-04-14 17:56:15 -0700

    Add test for resetting buffer before render

--- test/index.js
+++ test/index.js
@@ -4,7 +4,23 @@ var styletron = require('styletron');
 var styletronServer = require('../');
 
 test('server rendering', function(t) {
-  styletron.reset();
+  var result = styletronServer.renderStatic(function mockRender() {
+    styletron.injectOnce('bar', '.bar {}');
+    return 'html contents';
+  });
+
+  t.equal(result.html, 'html contents');
+  t.equal(result.css, '.bar {}');
+  t.equal(result.hydrationSrc,
+    ';try {window["__GLOBAL_STYLETRON_HYDRATE@1__"] = ["bar"];} catch(e) {};');
+  t.end();
+});
+
+test('reset before render', function(t) {
+  // inject something before
+  styletron.startBuffering();
+  styletron.injectOnce('foo', '.foo {}');
+
   var result = styletronServer.renderStatic(function mockRender() {
     styletron.injectOnce('bar', '.bar {}');
     return 'html contents';
